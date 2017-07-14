756cc45e3e069cfc5bfdfa5f2e1b9beeaf59da4f
YaminaBoghossian <yamina.boghossian@gmail.com>
2017-07-14 13:34:46 +0200

    change

--- index.php
+++ index.php
@@ -9,13 +9,13 @@
     <title>index</title>
     <style>
         @font-face {
-            font-family: "OldStandard-Regular";
-            src: url('fonts/OldStandard-Regular.ttf');
+            font-family: 'SourceSansPro-ExtraLight';
+            src: url('font/SourceSansPro-ExtraLight.ttf');
         }
         
         @font-face {
-            font-family: "OldStandard-Italic";
-            src: url('fonts/OldStandard-Italic.ttf');
+            font-family: "OldStandard-Regular";
+            src: url('fonts/OldStandard-Regular.ttf');
         }
         
         @font-face {
@@ -35,12 +35,12 @@
         
         header {
             width: 100%;
-            background: rgba(254, 253, 253, 0.2);
+            background: rgba(254, 253, 253, 1);
             position: fixed;
             top: 0;
             left: 0;
             right: 0;
-            height: 100px;
+            height: 120px;
             padding: 5px;
             padding-left: 5px;
             padding-top: 0;
@@ -62,7 +62,7 @@
         }
         
         h6 {
-            font-family: 'OldStandard-Bold';
+            font-family: 'SourceSansPro-ExtraLight';
             font-size: 20px;
             color: #FF5E83;
             text-align: center;
@@ -127,7 +127,9 @@
             margin-bottom: 220px;
             display: flex;
             flex-direction: row;
+            justify-content: space-around;
             margin: auto;
+
            
         }
 
@@ -199,7 +201,7 @@
         }
         
         .plilfooter {
-            font-family: 'OldStandard-Regular';
+            font-family: 'SourceSansPro-Light';
             font-size: 1em;
             color: white;
             margin: auto;
@@ -207,7 +209,7 @@
 
 
     .screenprojet {
-    width: 75%;
+    width: 82%;
     height: 300px;
     margin: auto;
     text-align: center;
@@ -228,7 +230,8 @@ h4 {
     font-size: 1.5em;
     color: white;
     text-align : center;
-    margin-bottom: 15px;
+    
+    margin: auto;
     
 }
 
@@ -242,13 +245,15 @@ img {
 }
 
 
+
 .details {
-    background: 
-#faab48;
-/*background:rgb(240, 149, 51);*/
+   /* background:
+
+#62dd1d;*/
+/*#bcf871;*/
+background:#7DDF64;
     /*background:#ff8a65;*/
     opacity: 0.8;
-    
     width: 24.8%;
     margin-top: 62px;
     margin:auto;
@@ -260,12 +265,122 @@ img {
     flex-direction: column;
 }
 h8 {
-    font-family:"OldStandard-Regular";
+    font-family:"SourceSansPro-Light";
     color:#102f2d;
     font-size:0.9em ;
     margin-left: 5px;
+    margin: auto;
+    
     
 }
+
+hr {
+    color: #102f2d;
+    background: white;
+}
+
+.back-to-top{
+  font-weight: bold;
+  text-decoration: none;
+  color: #fff;
+  background:
+grey;
+  height: 50px;
+  width: 50px;
+  display: inline-block;
+  text-align: center;
+  line-height: 60px;
+  font-size: 30px;
+  
+  position: fixed;;
+  bottom: 65px;
+  right: 10px;
+}
+
+/*
+.box {
+  width: 40%;
+  margin: 0 auto;
+  background: rgba(255,255,255,0.2);
+  padding: 35px;
+  
+  border-radius: 20px/50px;
+  background-clip: padding-box;
+  text-align: center;
+}*/
+
+
+.overlay {
+  position: fixed;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  background: rgba(0, 0, 0, 0.7);
+  transition: opacity 500ms;
+  visibility: hidden;
+  opacity: 0;
+}
+.overlay:target {
+  visibility: visible;
+  opacity: 1;
+}
+
+.popup {
+margin: 80px auto;
+  
+  background: grey ;
+  
+  
+  width: 80%;
+  height: 400px;
+  position: relative;
+  transition: all 5s ease-in-out;
+  padding-left: 35px;
+  padding-right: 35px;
+  padding-top: 35px;
+  padding-bottom: 50px;
+  
+  
+}
+
+.popup h2 {
+  margin-top: 20px;
+  color: white;
+  font-family: "JuliusSansOne-Regular";
+  
+}
+.popup .close {
+  position: absolute;
+  top: 20px;
+  right: 30px;
+  transition: all 200ms;
+  font-size: 30px;
+  font-weight: bold;
+  text-decoration: none;
+  color: #333;
+}
+.popup .close:hover {
+  color: #06D85F;
+}
+.popup .content {
+  max-height: 90%;
+  overflow: auto;
+  color: white;
+  font-family: 'SourceSansPro-Light';
+  letter-spacing: 2px;
+  word-spacing: 3px;
+}
+
+@media screen and (max-width: 700px){
+  .box{
+    width: 70%;
+  }
+  .popup{
+    width: 70%;
+  }
+}
+
 /* divs background appearing with hover */ 
 
 
@@ -275,17 +390,53 @@ h8 {
 <body>
     <header>
         <h1> Yamina Boghossian </h1>
-
-
-        <br/>
+<br/>
         <hr size=2 align=center width="20%">
     </header>
 
     <div class='introduction'>
-        <h3> Welcome on my page </h3>
+        <h3> Welcome to my page </h3>
         <h5>Here's an outlook of my work as a developper student in Lyon, France.</h5>
+<div class="box">
+	 <a href="#popup1"><button class="btn">About</button></a>
+</div>
 
-        <button class="btn">About</button>
+<div id="popup1" class="overlay">
+           
+	<div class="popup">
+		<h2>Skills </h2>
+
+		<a class="close" href="#">&times;</a>
+        
+		<div class="content">
+            Front-End <br/>
+            HTML - CSS - SASS - JAVASCRIPT - JQUERY - UX DESIGN - BOOTSTRAP<br/><br/>
+            Back-End <br/>
+            PHP - SQL
+            <br/><br/><br/>
+        <h2> Experience </h2>
+        <div class='content'>
+            High school diploma in literature and English <br/>
+            <br/>Studied 2 years in Marketing / Advertising / Management<br/>
+            Commercial background +6 years (sales assistance, mershandising ) <br/>
+            Food and beverage +5 years 
+            Lorem ipsum dolor sit amet, consectetur 
+            adipisicing elit. Magni fuga nam dolores numquam delectus dignissimos 
+            voluptas iste molestias amet tempore cupiditate sint, deserunt iusto provident, animi itaque natus harum. Similique.
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Debitis sit possimus nulla laboriosam sapiente, dolorem, rem voluptatibus esse neque sed atque ipsam quam architecto perferendis veritatis et, ab minima exercitationem.
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet soluta, tempore rerum nulla. Laudantium quidem, voluptate consectetur eaque quia, hic in atque facere, voluptas quo tenetur accusantium dolor dolore iste.
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Impedit ipsam omnis soluta veniam, provident praesentium reiciendis velit, vero amet consequatur repudiandae tempore laudantium quasi. Necessitatibus quam inventore dignissimos, corrupti hic.
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci nesciunt dolores animi autem assumenda optio sit unde officiis et dolor, impedit maxime nisi aperiam explicabo eveniet asperiores, expedita sapiente sed.
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rem provident fuga adipisci odio porro eaque excepturi voluptates sed molestiae reprehenderit. Cum aliquid exercitationem aliquam obcaecati veniam animi earum, laudantium inventore?
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vero a alias deleniti dolorem. Explicabo rem exercitationem temporibus, ex sed placeat natus molestias adipisci. Temporibus eum sapiente sunt nobis impedit modi!
+            </div>
+         
+    
+			
+		</div>
+	</div>
+</div>
+        
 
     </div>
     <div class='portfolio'>
@@ -311,6 +462,7 @@ $dossier ="./";
     
        <div class="screenprojet"> 
        <div class="details"><h4>'.$title.' </h4>
+<hr size=1 align=center width="10%">
         <h8>'.$comment.'</h8> </div>
        <img src="'.$screen.'"> </div>
    </a>';
@@ -331,8 +483,12 @@ $dossier ="./";
             <p class='pcontact'>(00 33) 6 62 51 46 48 <br/> <br/> Yamina.boghossian<span>@</span>gmail.com </p>
 
         </div>
+<div class="filler">
 
+</div>
+<a href="#" class="back-to-top">^</a>
     </footer>
+    
     <div class='lilfooter'>
         <p class='plilfooter'> ©Yamina Boghossian | 2017 </p>
     </div>
